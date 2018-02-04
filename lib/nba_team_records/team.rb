f02d50765fef326f786a6540cb741ea3ee530a08
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-02-04 19:02:31 +0000

    get team attributes from scraping espn

--- lib/nba_team_records/team.rb
+++ lib/nba_team_records/team.rb
@@ -75,10 +75,27 @@ class NbaTeamRecords::Team
     # team_1.opp = doc.css('.standings-row').first.css('td')[10].text
     # team_1.diff = doc.css('.standings-row').first.css('td')[11].text
     # team_1.streak = doc.css('.standings-row').first.css('td')[12].text
-    # team_1.l10 = doc.css('.standings-row').first.css('td')[12].text
+    # team_1.l10 = doc.css('.standings-row').first.css('td')[13].text
 
     doc = Nokogiri::HTML(open("http://espn.com/nba/standings/_/group/league"))
-    binding.pry
+    doc.css('.standings-row').map do |team_name|
+      team = self.new
+      team.name = stats.css('.team-names').first.text
+      team.wins = stats.css('td')[1].text
+      team.loses = stats.css('td')[2].text
+      team.pct = stats.css('td')[3].text
+      team.gb = stats.css('td')[4].text
+      team.home = stats.css('td')[5].text
+      team.road = stats.css('td')[6].text
+      team.div = stats.css('td')[7].text
+      team.conf = stats.css('td')[8].text
+      team.ppg = stats.css('td')[9].text
+      team.opp = stats.css('td')[10].text
+      team.diff = stats.css('td')[11].text
+      team.streak = stats.css('td')[12].text
+      team.l10 = stats.css('td')[13].text
+      team
+    end
   end
 
 end
