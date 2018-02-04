8119fa370ec5500fff30bc82dc6866a11b92032b
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-02-04 15:23:32 +0000

    added list teams method to NbaTeamRecords::CLI class

--- lib/nba_team_records/cli.rb
+++ lib/nba_team_records/cli.rb
@@ -2,6 +2,18 @@ class NbaTeamRecords::CLI
 
   def call
     puts "NBA Team Stats:"
+    list_teams
+  end
+
+  def list_teams
+    # get list of nba teams
+    puts <<~HEREDOC
+    1. Golden State Warriors
+    2. Houston Rockets
+    3. Boston Celtics
+    4. Toronto Raptors
+    5. San Antonio Spurs
+    HEREDOC
   end
 
 end
