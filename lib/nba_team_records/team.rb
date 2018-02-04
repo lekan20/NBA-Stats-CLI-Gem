fbe6b444cfb87841eb0588236cd8a53f92ba86cc
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-02-04 17:11:15 +0000

    Got non scraped objects to work

--- lib/nba_team_records/team.rb
+++ lib/nba_team_records/team.rb
@@ -1,18 +1,19 @@
 class NbaTeamRecords::Team
-  attr_accessor :wins, :loses, :pct, :gb, :home, :road, :div, :conf, :ppg, :opp, :diff, :streak, :l10
+  attr_accessor :name, :wins, :loses, :pct, :gb, :home, :road, :div, :conf, :ppg, :opp, :diff, :streak, :l10
 
   def self.all
     # returns all the teams
     puts "NBA Team Stats:"
-    puts <<~HEREDOC
-    1. Golden State Warriors
-    2. Houston Rockets
-    3. Boston Celtics
-    4. Toronto Raptors
-    5. San Antonio Spurs
-    HEREDOC
+    # puts <<~HEREDOC
+    # 1. Golden State Warriors
+    # 2. Houston Rockets
+    # 3. Boston Celtics
+    # 4. Toronto Raptors
+    # 5. San Antonio Spurs
+    # HEREDOC
 
     team_1 = self.new
+    team_1.name = "Golden State Warriors"
     team_1.wins = "40"
     team_1.loses = "10"
     team_1.pct = ".800"
@@ -28,9 +29,10 @@ class NbaTeamRecords::Team
     team_1.l10 = "8-2"
 
     team_2 = self.new
+    team_2.name = "Houston Rockets"
     team_2.wins = "38"
     team_2.loses = "13"
-    team_2.win pct = ".745"
+    team_2.pct = ".745"
     team_2.gb = "2"
     team_2.home = "20-6"
     team_2.road = "18-7"
