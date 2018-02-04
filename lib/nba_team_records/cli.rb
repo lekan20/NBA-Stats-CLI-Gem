3cb8a8b741479990becedbda2c514aac2907d08f
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-02-04 17:37:20 +0000

    Add outputs I want from user input in stats method

--- lib/nba_team_records/cli.rb
+++ lib/nba_team_records/cli.rb
@@ -18,7 +18,23 @@ class NbaTeamRecords::CLI
       puts "Enter the number of the team you want the stats of, type list to see the team list, or write exit to exit:"
       input = gets.strip.downcase
       if input.to_i > 0
-        puts @team[input.to_i-1]
+        the_team = @team[input.to_i-1]
+        puts <<~HEREDOC
+        Team: #{the_team.name}
+        Wins: #{the_team.wins}
+        Loses: #{the_team.loses}
+        PCT: #{the_team.pct}
+        GB: #{the_team.gb}
+        Home:#{the_team.home}
+        Road: #{the_team.road}
+        Div:#{the_team.div}
+        Conf: #{the_team.conf}
+        PPG: #{the_team.ppg}
+        OPP PPG:#{the_team.opp}
+        Diff:#{the_team.diff}
+        Streak: #{the_team.streak}
+        L10: #{the_team.l10}
+        HEREDOC
       elsif input == "list"
         list_deals
       else
