4d18450530197049d15e10b55079100dd807069d
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-02-04 15:47:59 +0000

    updated list teams method with logic and added goodbye method

--- lib/nba_team_records/cli.rb
+++ lib/nba_team_records/cli.rb
@@ -1,12 +1,14 @@
 class NbaTeamRecords::CLI
 
   def call
-    puts "NBA Team Stats:"
     list_teams
+    stats
+    goodbye
   end
 
   def list_teams
     # get list of nba teams
+    puts "NBA Team Stats:"
     puts <<~HEREDOC
     1. Golden State Warriors
     2. Houston Rockets
@@ -14,6 +16,26 @@ class NbaTeamRecords::CLI
     4. Toronto Raptors
     5. San Antonio Spurs
     HEREDOC
+  end
+
+  def stats
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the team you want the stats of, type list to see the team list, or write exit to exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Team 1 stats"
+      when "2"
+        puts "Team 2 stats"
+      when "list"
+        list_teams
+      end
+    end
+  end
+
+  def goodbye
+    puts "Start me up again the next time you want to know your team's stats!!!!"
   end
 
 end
